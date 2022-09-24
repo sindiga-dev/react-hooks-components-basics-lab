2c1654dac4cd2c2652f34ddb8c51f66d96857a7d
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-09-24 17:42:17 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,42 @@
 import React from "react";
 
 function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
+	return (
+		<nav>
+			<a href="#home">I'm a link!</a>
+		</nav>
+	);
 }
 
 function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
+	return (
+		<div id="home">
+			<h1>Home</h1>
+		</div>
+	);
 }
 
-{/* write an <About> component here */}
-
+{
+	/* write an <About> component here */
+}
+const About = () => {
+	return (
+		<div id="about">
+			<h1>About</h1>
+		</div>
+	);
+};
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
-    </div>
-  );
+	return (
+		<div>
+			<NavBar />
+			{/* add the <Home> component here */}
+			<Home />
+			{/* add your <About> component here */}
+			<About />
+		</div>
+	);
 }
 
 export default App;
